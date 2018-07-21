c296db1b6a16f9d90b24c1e155acf5831ac9fbfb
Trevor <trevku155@gmail.com>
2018-07-21 12:23:28 -0400

    problem 30 - fifth digit powers

--- problem_solutions/problem_030.py
+++ problem_solutions/problem_030.py
@@ -2,10 +2,21 @@
 Problem 30 - Digit Fifth Powers
 https://projecteuler.net/problem=30
 """
+import math
 
 
 def fifth_powers():
-    return -1
+    nums = []
+    # each digit can give you at max 9^5 to your sum, so we don't have to iterate through anything past 6 digits
+    for i in range(2, 300000):
+        i_str = str(i)
+        power_sum = 0
+        for c in i_str:
+            power_sum += int(math.pow(int(c), 5))
+        if i == power_sum:
+            nums.append(i)
+    # print(nums)
+    return sum(nums)
 
 
 if __name__ == "__main__":
