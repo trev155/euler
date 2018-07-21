c3789b71193d1a28dc434ebe21c779561fc9ff22
Trevor <trevku155@gmail.com>
2018-07-21 11:59:19 -0400

    remove unused function

--- problem_solutions/problem_029.py
+++ problem_solutions/problem_029.py
@@ -14,14 +14,6 @@ def distinct_powers():
     return len(s)
 
 
-def generate_powers(a):
-    lst = []
-    for i in range(2, 101):
-        n = int(math.pow(a, i))
-        lst.append(n)
-    return lst
-
-
 if __name__ == "__main__":
     answer = distinct_powers()
     print(answer)
